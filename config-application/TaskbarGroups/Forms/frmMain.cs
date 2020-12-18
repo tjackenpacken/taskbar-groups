9e1cb7d4f425707fba68cab31556c4dca0509884
Jack Schierbeck <jack.schierbeck@gmail.com>
2020-12-18 17:42:59 +0100

    support for vertical and top taskar and support for multiple monitors

--- config-application/TaskbarGroups/Forms/frmMain.cs
+++ config-application/TaskbarGroups/Forms/frmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using TaskbarGroups.Classes;
@@ -18,18 +19,107 @@ namespace TaskbarGroups
         {
             Category category = new Category(@"ObjectData.xml");
             LoadCategory(category);
-
-
+            SetLocation();
+        }
+        
+        private void SetLocation()
+        {
             POINT p = new POINT();
             NativeMethods.GetCursorPos(ref p); // catch cursor location
+            List<Rectangle> taskbarList = FindDockedTaskBars();
+            Rectangle taskbar = new Rectangle();
+            Rectangle screen = new Rectangle();
+
+            System.Drawing.Point mouseClick = new System.Drawing.Point(p.x, p.y);
+            int i = 0;
             int locationy;
-            if (p.y > Screen.PrimaryScreen.Bounds.Height - 35)
-                locationy = Screen.PrimaryScreen.Bounds.Height - this.Height - 45;
-            else
-                locationy = p.y - this.Height - 20;
-            int locationx = p.x - (this.Width / 2);
-            this.Location = new Point
-                (locationx, locationy); // set location to cursor x and bot y
+            int locationx;
+            if (taskbarList.Count != 0) // if taskbar is not hidden
+            {
+                foreach (var scr in Screen.AllScreens) // get what screen user clicked on
+                {
+                    if (scr.Bounds.Contains(mouseClick))
+                    {
+                        screen.X = scr.Bounds.X;
+                        screen.Y = scr.Bounds.Y;
+                        screen.Width = scr.Bounds.Width;
+                        screen.Height = scr.Bounds.Height;
+                        taskbar = taskbarList[i];
+                    }
+                    i++;
+                }
+
+                if (taskbar.Contains(mouseClick)) // click on taskbar
+                {
+                    if (taskbar.Top == screen.Top && taskbar.Width == screen.Width)
+                    {
+                        // TOP
+                        locationy = screen.Y + taskbar.Height + 10;
+                        locationx = p.x - (this.Width / 2);
+                    }
+                    else if (taskbar.Bottom == screen.Bottom && taskbar.Width == screen.Width)
+                    {
+                        // BOTTOM
+                        locationy = screen.Y + screen.Height - this.Height - taskbar.Height - 10;
+                        locationx = p.x - (this.Width / 2);
+                    }
+                    else if (taskbar.Left == screen.Left)
+                    {
+                        // LEFT
+                        locationy = p.y - (this.Height / 2);
+                        locationx = screen.X + taskbar.Width + 10;
+
+                    }
+                    else
+                    {
+                        // RIGHT
+                        locationy = p.y - (this.Height / 2);
+                        locationx = screen.X + screen.Width - this.Width - taskbar.Width - 10;
+                    }
+
+                }
+                else // not click on taskbar
+                {
+                    locationy = p.y - this.Height - 20;
+                    locationx = p.x - (this.Width / 2);
+                }
+
+                this.Location = new Point(locationx, locationy);
+                if (this.Left < screen.Left)
+                    this.Left = screen.Left + 10;
+                if (this.Top < screen.Top)
+                    this.Top = screen.Top + 10;
+                if (this.Right > screen.Right)
+                    this.Left = screen.Right - this.Width - 10;
+            }
+            else // if taskbar is hidden
+            {
+                foreach (var scr in Screen.AllScreens) // get what screen user clicked on
+                {
+                    if (scr.Bounds.Contains(mouseClick))
+                    {
+                        screen.X = scr.Bounds.X;
+                        screen.Y = scr.Bounds.Y;
+                        screen.Width = scr.Bounds.Width;
+                        screen.Height = scr.Bounds.Height;
+                    }
+                    i++;
+                }
+
+                if (p.y > Screen.PrimaryScreen.Bounds.Height - 35)
+                    locationy = Screen.PrimaryScreen.Bounds.Height - this.Height - 45;
+                else
+                    locationy = p.y - this.Height - 20;
+                locationx = p.x - (this.Width / 2);
+
+                this.Location = new Point(locationx, locationy);
+                if (this.Left < screen.Left)
+                    this.Left = screen.Left + 10;
+                if (this.Top < screen.Top)
+                    this.Top = screen.Top + 10;
+                if (this.Right > screen.Right)
+                    this.Left = screen.Right - this.Width - 10;
+            }
         }
 
         private void LoadCategory(Category category)
@@ -41,7 +131,7 @@ namespace TaskbarGroups
             int width = category.Width;
             int columns = 1;
 
-            foreach (ProgramShortcut shortcut in category.ShortcutList)
+            foreach (ProgramShortcut psc in category.ShortcutList)
             {
 
                 if (columns > width)  // creating new row if there are more psc than max width
@@ -55,24 +145,28 @@ namespace TaskbarGroups
                 if (this.Width < ((width * 50)))
                     this.Width += (25 + 25);
 
-                // building psc panel
-                this.shortcutPanel = new System.Windows.Forms.PictureBox();
-                this.shortcutPanel.BackColor = System.Drawing.Color.Transparent;
-                this.shortcutPanel.Location = new System.Drawing.Point(x, y);
-                this.shortcutPanel.Size = new System.Drawing.Size(25,25);
-                this.shortcutPanel.BackgroundImage = System.Drawing.Icon.ExtractAssociatedIcon(shortcut.FilePath).ToBitmap();
-                this.shortcutPanel.BackgroundImageLayout = ImageLayout.Stretch;
-                this.shortcutPanel.TabStop = false;
-                this.shortcutPanel.Click += new System.EventHandler((sender, e) => OpenFile(sender, e, shortcut.FilePath));
-                this.shortcutPanel.Cursor = System.Windows.Forms.Cursors.Hand;
-                this.Controls.Add(this.shortcutPanel);
-                this.shortcutPanel.Show();
-                this.shortcutPanel.BringToFront();
-
+                BuildShortcutPanel(x, y, psc);
                 x += 50;
                 columns++;
             }
         }
+
+        private void BuildShortcutPanel(int x, int y, ProgramShortcut psc)
+        {
+            this.shortcutPanel = new System.Windows.Forms.PictureBox();
+            this.shortcutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.shortcutPanel.Location = new System.Drawing.Point(x, y);
+            this.shortcutPanel.Size = new System.Drawing.Size(25, 25);
+            this.shortcutPanel.BackgroundImage = System.Drawing.Icon.ExtractAssociatedIcon(psc.FilePath).ToBitmap();
+            this.shortcutPanel.BackgroundImageLayout = ImageLayout.Stretch;
+            this.shortcutPanel.TabStop = false;
+            this.shortcutPanel.Click += new System.EventHandler((sender, e) => OpenFile(sender, e, psc.FilePath));
+            this.shortcutPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Controls.Add(this.shortcutPanel);
+            this.shortcutPanel.Show();
+            this.shortcutPanel.BringToFront();
+        }
+
 
         private void OpenFile(object sender, EventArgs e, string path)
         {
@@ -89,6 +183,64 @@ namespace TaskbarGroups
             {
                 MessageBox.Show(Ex.Message);
             }
+        }
+
+        public static List<Rectangle> FindDockedTaskBars()
+        {
+            List<Rectangle> dockedRects = new List<Rectangle>();
+            foreach (var tmpScrn in Screen.AllScreens)
+            {
+                if (!tmpScrn.Bounds.Equals(tmpScrn.WorkingArea))
+                {
+                    Rectangle rect = new Rectangle();
+
+                    var leftDockedWidth = Math.Abs((Math.Abs(tmpScrn.Bounds.Left) - Math.Abs(tmpScrn.WorkingArea.Left)));
+                    var topDockedHeight = Math.Abs((Math.Abs(tmpScrn.Bounds.Top) - Math.Abs(tmpScrn.WorkingArea.Top)));
+                    var rightDockedWidth = ((tmpScrn.Bounds.Width - leftDockedWidth) - tmpScrn.WorkingArea.Width);
+                    var bottomDockedHeight = ((tmpScrn.Bounds.Height - topDockedHeight) - tmpScrn.WorkingArea.Height);
+                    if ((leftDockedWidth > 0))
+                    {
+                        rect.X = tmpScrn.Bounds.Left;
+                        rect.Y = tmpScrn.Bounds.Top;
+                        rect.Width = leftDockedWidth;
+                        rect.Height = tmpScrn.Bounds.Height;
+                    }
+                    else if ((rightDockedWidth > 0))
+                    {
+                        rect.X = tmpScrn.WorkingArea.Right;
+                        rect.Y = tmpScrn.Bounds.Top;
+                        rect.Width = rightDockedWidth;
+                        rect.Height = tmpScrn.Bounds.Height;
+                    }
+                    else if ((topDockedHeight > 0))
+                    {
+                        rect.X = tmpScrn.WorkingArea.Left;
+                        rect.Y = tmpScrn.Bounds.Top;
+                        rect.Width = tmpScrn.WorkingArea.Width;
+                        rect.Height = topDockedHeight;
+                    }
+                    else if ((bottomDockedHeight > 0))
+                    {
+                        rect.X = tmpScrn.WorkingArea.Left;
+                        rect.Y = tmpScrn.WorkingArea.Bottom;
+                        rect.Width = tmpScrn.WorkingArea.Width;
+                        rect.Height = bottomDockedHeight;
+                    }
+                    else
+                    {
+                        // Nothing found!
+                    }
+
+                    dockedRects.Add(rect);
+                }
+            }
+
+            if (dockedRects.Count == 0)
+            {
+                // Taskbar is set to "Auto-Hide".
+            }
+
+            return dockedRects;
         }
 
         private void frmMain_Deactivate(object sender, EventArgs e)
