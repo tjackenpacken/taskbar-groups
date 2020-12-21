0e5b50e27978128532b10a44bd9af72f61fee642
Jack Schierbeck <jack.schierbeck@gmail.com>
2020-12-21 21:08:43 +0100

    remade solution architecture

--- main/client.cs
+++ main/client.cs
@@ -17,7 +17,7 @@ namespace main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmClient());
+            Application.Run(new frmClient()); 
         }
     }
 }
