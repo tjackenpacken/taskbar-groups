353432dcfb7273733e4b4ea9195eb9e6396efee7
BanCrash <BanCrash@users.noreply.github.com>
2022-05-23 11:33:55 +0200

    Fixed error "This program does not have access...
    ... to this directory"

--- main/Classes/Paths.cs
+++ main/Classes/Paths.cs
@@ -13,16 +13,16 @@ namespace client.Classes
     static class Paths
     {
         /// <summary>
-        /// The folder in which the main executable resides.
-        /// </summary>
-        public static String path = System.IO.Path.GetDirectoryName(exeString);
-
-        /// <summary>
         /// The full path to the application's main executable.
         /// </summary>
         public static String exeString = System.Reflection.Assembly.GetExecutingAssembly().Location;
 
         public static String exeFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+        /// <summary>
+        /// The folder in which the main executable resides.
+        /// </summary>
+        public static String path = System.IO.Path.GetDirectoryName(exeString);
 
         public static String defaultConfigPath;
         public static String defaultShortcutsPath;
